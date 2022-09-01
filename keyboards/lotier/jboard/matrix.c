d6f8e309f142f7c6c99a92f60bb4d33538e15e28
Jon <Lotier@users.noreply.github.com>
2022-08-31 21:38:21 -0400

    Bluetooth working, but buggy
    Bluetooth is working but it's a bit buggy, gotta hold the key for it to work.

--- keyboards/lotier/jboard/matrix.c
+++ keyboards/lotier/jboard/matrix.c
@@ -1,17 +1,10 @@
 #include <stdint.h>
-#include <stdbool.h>
-#include "wait.h"
-#include "util.h"
-#include "matrix.h"
 #include "quantum.h"
 #include "i2c_master.h"
-#include "print.h"
 
 #define SX1509_ADDR 0x3E << 1 // Configurable with jumpers
 #define SX1509_TIMEOUT 200 // milliseconds
 
-#define DATAA 0x11
-#define DATAB 0x10
 enum sx1509_registers {
   RegReset = 0x7d,
 	RegDirA = 0x0f,
@@ -23,87 +16,112 @@ enum sx1509_registers {
 };
 
 static i2c_status_t sx1509_status;
+static bool sx1509_init = false;
+static uint8_t reinit_counter;
+
 static const pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
 
-/* matrix state(1:on, 0:off) */
-extern matrix_row_t raw_matrix[MATRIX_ROWS];  // raw values
-extern matrix_row_t matrix[MATRIX_ROWS];      // debounced values
-
 static void select_row(uint8_t row) {
-    setPinOutput(row_pins[row]);
-    writePinLow(row_pins[row]);
+  setPinOutput(row_pins[row]);
+  writePinLow(row_pins[row]);
 }
 
-static void unselect_row(uint8_t row) { setPinInputHigh(row_pins[row]); }
+static void unselect_row(uint8_t row) { 
+  setPinInputHigh(row_pins[row]); 
+}
 
 static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row) {
-    // Store last value of row prior to reading
-    matrix_row_t last_row_value = current_matrix[current_row];
+  // Store last value of row prior to reading
+  matrix_row_t last_row_value = current_matrix[current_row];
 
-    // Clear data in matrix row
-    current_matrix[current_row] = 0;
+  if(!sx1509_init) {
+    goto done;
+  }
 
-    uint8_t buf[2];
+  // Clear data in matrix row
+  current_matrix[current_row] = 0;
 
-    uint8_t data = DATAB;
+  // Set up for reading SX1509 pin data.
+  uint8_t buf[2];
+  uint8_t data = DataB;
 
-    // Select row and wait for row selecton to stabilize
-    select_row(current_row);
-    wait_us(30);
+  // Select row and wait for row selecton to stabilize
+  select_row(current_row);
+  wait_us(30);
 
-    // For each col...
-    // for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++) {
-    //     // Select the col pin to read (active low)
-    //     uint8_t pin_state = readPin(col_pins[col_index]);
+  sx1509_status = i2c_start(SX1509_ADDR, SX1509_TIMEOUT);
+  if (sx1509_status != I2C_STATUS_SUCCESS) { sx1509_init = false; goto done; }
+  sx1509_status = i2c_transmit(SX1509_ADDR, &data, 1, SX1509_TIMEOUT);
+  if (sx1509_status != I2C_STATUS_SUCCESS) { sx1509_init = false; goto done; }
+  sx1509_status = i2c_receive(SX1509_ADDR, buf, 2, SX1509_TIMEOUT);
+  if (sx1509_status != I2C_STATUS_SUCCESS) { sx1509_init = false; goto done; }
 
-    //     // Populate the matrix row with the state of the col pin
-    //     current_matrix[current_row] |= pin_state ? 0 : (MATRIX_ROW_SHIFTER << col_index);
-    // }
-    sx1509_status = i2c_start(SX1509_ADDR, SX1509_TIMEOUT);
-    if (sx1509_status != I2C_STATUS_SUCCESS) { print("error setting start"); }
-    sx1509_status = i2c_transmit(SX1509_ADDR, &data, 1, SX1509_TIMEOUT);
-    if (sx1509_status != I2C_STATUS_SUCCESS) { print("error setting transmit"); }
-    sx1509_status = i2c_receive(SX1509_ADDR, buf, 2, SX1509_TIMEOUT);
-    if (sx1509_status != I2C_STATUS_SUCCESS) { print("error setting receive"); }
+  done:
     i2c_stop();
 
     // Unselect row
     unselect_row(current_row);
 
-    current_matrix[current_row] = ~((buf[0] << 8) | buf[1]);
+    if (!sx1509_init) {
+      dprint("failed to read SX1509, will re-init.\n");
+    } else {
+      current_matrix[current_row] = ~((buf[0] << 8) | buf[1]);
+    }
 
     return (last_row_value != current_matrix[current_row]);
 }
 
-void matrix_init_custom(void) {
+void sx1509_startup(void) {
   uint8_t reset[] = {0x12, 0x34};
   uint8_t allOn = 0xff;
+
+  sx1509_init = false;
+  
   // Software reset
   sx1509_status = i2c_writeReg(SX1509_ADDR, RegReset, &reset[0], 1, SX1509_TIMEOUT);
-  if (sx1509_status != I2C_STATUS_SUCCESS) { print("error setting reset 1"); }
+  if (sx1509_status != I2C_STATUS_SUCCESS) { sx1509_init = false; goto done; }
   sx1509_status = i2c_writeReg(SX1509_ADDR, RegReset, &reset[1], 1, SX1509_TIMEOUT);
-  if (sx1509_status != I2C_STATUS_SUCCESS) { print("error setting reset 2"); }
+  if (sx1509_status != I2C_STATUS_SUCCESS) { sx1509_init = false; goto done; }
 
   // Set all the pins as inputs
   sx1509_status = i2c_writeReg(SX1509_ADDR, RegDirA, &allOn, 1, SX1509_TIMEOUT);
-  if (sx1509_status != I2C_STATUS_SUCCESS) { print("error setting writing 1"); }
+  if (sx1509_status != I2C_STATUS_SUCCESS) { sx1509_init = false; goto done; }
   sx1509_status = i2c_writeReg(SX1509_ADDR, RegDirB, &allOn, 1, SX1509_TIMEOUT);
-  if (sx1509_status != I2C_STATUS_SUCCESS) { print("error setting writing 2"); }
-
+  if (sx1509_status != I2C_STATUS_SUCCESS) { sx1509_init = false; goto done; }
 
   // Turn on internal pull-ups
   sx1509_status = i2c_writeReg(SX1509_ADDR, RegPullUpA, &allOn, 1, SX1509_TIMEOUT);
-  if (sx1509_status != I2C_STATUS_SUCCESS) { print("error setting pullup 1"); }
+  if (sx1509_status != I2C_STATUS_SUCCESS) { sx1509_init = false; goto done; }
   sx1509_status = i2c_writeReg(SX1509_ADDR, RegPullUpB, &allOn, 1, SX1509_TIMEOUT);
-  if (sx1509_status != I2C_STATUS_SUCCESS) { print("error setting pullup 1"); }
+  if (sx1509_status != I2C_STATUS_SUCCESS) { sx1509_init = false; goto done; } 
+
+  sx1509_init = true;
+
+  done:
+    if (!sx1509_init) {
+      dprint("Failed to init SX1509.\n");
+    } else {
+      dprint("SX1509 initialized!\n");
+    }
+}
+
+void matrix_init_custom(void) {
+  sx1509_startup();
 }
 
 bool matrix_scan_custom(matrix_row_t current_matrix[]) {
     bool matrix_has_changed = false;
 
+    if (!sx1509_init) {
+      // Will be true on every overflow event, aka 255 matrix scans
+      if (reinit_counter++ != 0) {
+        sx1509_startup();
+      }
+    }
+
     // Set row, read cols
     for (uint8_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
-        matrix_has_changed |= read_cols_on_row(raw_matrix, current_row);
+      matrix_has_changed |= read_cols_on_row(current_matrix, current_row);
     }
 
     return matrix_has_changed;
--- keyboards/lotier/jboard/rules.mk
+++ keyboards/lotier/jboard/rules.mk
@@ -9,17 +9,18 @@ BOOTLOADER = caterina
 # Build Options
 #   change yes to no to disable
 #
-ADAFRUIT_BLE_ENABLE = no    # Enable Bluetooth with the Adafruit BLE boards
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+BLUETOOTH_ENABLE = yes
+BLUETOOTH_DRIVER = BluefruitLE
+
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
+CONSOLE_ENABLE = yes         # Console for debug
+COMMAND_ENABLE = yes         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-
 
 CUSTOM_MATRIX = lite        # We have a custom matrix
 SRC = matrix.c
